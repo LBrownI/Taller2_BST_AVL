a3bc30ff78f0d0e29cb0deee02a13d169e39d7e8
alan <asilvar4@correo.uss.cl>
2024-06-08 22:45:51 -0400

    show parent and child connections

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 class ListaEnlazada {
     Nodo cabeza;
@@ -54,43 +55,53 @@ class Nodo {
 public class Main {
 
     static void imprimirLista(ListaEnlazada lista) {
-        Nodo actual = lista.cabeza;
-        ArrayList<Nodo[]> a = new ArrayList<>();
-        Nodo[] b = new Nodo[1];
-        b[0] = actual;
-        a.add(b);
-
-        System.out.println(b[0].value);
-
-        Nodo[] c = new Nodo[2];
-        c[0] = a.getFirst()[0].left_child;
-        c[1] = a.getFirst()[0].right_child;
-
-        a.add(c);
-
-        System.out.println(Arrays.toString(a.get(0)));
-        System.out.println(Arrays.toString(a.get(1)));
-
-//        while (actual.left_child != null) {
-//            actual = actual.left_child;
-//            System.out.println(actual.value);
-//        }
+        List<String> result = new ArrayList<>();
+        preOrder(lista.cabeza, result);
+        for (String line : result) {
+            System.out.println(line);
+        }
     }
-//            System.out.print(actual.valor + " ");
-//            actual = actual.siguiente;
-//        }
-//        System.out.println();
 
-    // something like this:
-    // [[1], [1, 2], [1, null, 2, 3]] (the index of the elements represents the height at that node.
-    // for each: actual.left_child, actual.right_child -> new list
+    static void preOrder(Nodo node, List<String> result) {
+        if (node == null) {
+            return;
+        }
+
+        String parentValue;
+        if (node.partner != null){
+            parentValue = String.valueOf(node.partner.value);
+        }
+        else {
+            parentValue = "NIL";
+        }
+
+        String nodeValue = String.valueOf(node.value);
+
+        String leftOrRightChild = "(root)";
+        if (node.partner != null){
+            if (node.value < node.partner.value){
+                leftOrRightChild = "(left)";
+            }
+            else {
+                leftOrRightChild = "(right)";
+            }
+        }
+
+        result.add("Parent: " + parentValue + " <--- Node: " + nodeValue + " " +leftOrRightChild);
+        preOrder(node.left_child, result);
+        preOrder(node.right_child, result);
+    }
+
 
     public static void main(String[] args) {
         ListaEnlazada lista = new ListaEnlazada();
-        lista.insertar(3);
-        lista.insertar(2);
+        lista.insertar(7);
+        lista.insertar(6);
         lista.insertar(4);
-        lista.insertar(1);
+        lista.insertar(5);
+        lista.insertar(9);
+        lista.insertar(8);
+        lista.insertar(11);
         imprimirLista(lista);
     }
 }
