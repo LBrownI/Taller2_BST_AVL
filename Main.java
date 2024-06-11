7ca063e007f7c398ca73fa76315529dba8af28ed
LBrownI <lucasbrowngd@gmail.com>
2024-06-11 03:06:28 -0400

    add delete and insert function for AVL tree

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import static java.lang.Math.max;
 
 class Node {
     int value;
@@ -97,7 +98,7 @@ class BinaryTree {
         if (x == null) {
             return -1;
         } else {
-            return 1 + Math.max(height(x.left), height(x.right));
+            return 1 + max(height(x.left), height(x.right));
         }
     }
 
@@ -159,7 +160,18 @@ class TreeAVL extends BinaryTree {
         }
         return N.height;
     }
-    private void rotateLeft(Node x) {
+    private void updateHeight(Node N) {
+        N.height = 1 + Math.max(height(N.left), height(N.right));
+    }
+
+    // Get Balance factor of node N
+    int getBalance(Node N)
+    {
+        if (N == null)
+            return 0;
+        return height(N.left) - height(N.right);
+    }
+    Node rotateLeft(Node x) {
         Node y = x.right;
         x.right = y.left;
         if (y.left != null) {
@@ -175,8 +187,11 @@ class TreeAVL extends BinaryTree {
         }
         y.left = x;
         x.parent = y;
+        updateHeight(x);
+        updateHeight(y);
+        return y;
     }
-    private void rotateRight(Node y) {
+    Node rotateRight(Node y) {
         Node x = y.left;
         y.left = x.right;
         if (x.right != null) {
@@ -192,7 +207,161 @@ class TreeAVL extends BinaryTree {
         }
         x.right = y;
         y.parent = x;
+        updateHeight(y);
+        updateHeight(x);
+        return x;
+    }
 
+    Node insert(Node node, int value) {
+        /* 1. Perform the normal BST rotation */
+        if (node == null) {
+            return (new Node(value));
+        }
+        if (value < node.value) {
+            node.left = insert(node.left, value);
+        } else if (value > node.value) {
+            node.right = insert(node.right, value);
+        } else { // Equal values not allowed
+            return node;
+        }
+        /* 2. Update height of this ancestor node */
+        node.height = 1 + max(height(node.left),
+                height(node.right));
+
+        /* 3. Get the balance factor of this ancestor
+        node to check whether this node became
+        Wunbalanced */
+        int balance = getBalance(node);
+
+        // If this node becomes unbalanced, then
+        // there are 4 cases Left Left Case
+        if (balance > 1 && value < node.left.value){
+            return rotateRight(node);
+        }
+        // Right Right Case
+        if (balance < -1 && value > node.right.value) {
+            return rotateLeft(node);
+        }
+        // Left Right Case
+        if (balance > 1 && value > node.left.value) {
+            node.left = rotateLeft(node.left);
+            return rotateRight(node);
+        }
+
+        // Right Left Case
+        if (balance < -1 && value < node.right.value) {
+            node.right = rotateRight(node.right);
+            return rotateLeft(node);
+        }
+        /* return the (unchanged) node pointer */
+        return node;
+    }
+
+    /* Given a non-empty binary search tree, return the
+    node with minimum value value found in that tree.
+    Note that the entire tree does not need to be
+    searched. */
+    Node minValueNode(Node node)
+    {
+        Node current = node;
+
+        /* loop down to find the leftmost leaf */
+        while (current.left != null)
+            current = current.left;
+
+        return current;
+    }
+
+    Node delete(Node root, int value)
+    {
+        // STEP 1: PERFORM STANDARD BST DELETE
+        if (root == null)
+            return root;
+
+        // If the value to be deleted is smaller than
+        // the root's value, then it lies in left subtree
+        if (value < root.value)
+            root.left = delete(root.left, value);
+
+            // If the value to be deleted is greater than the
+            // root's value, then it lies in right subtree
+        else if (value > root.value)
+            root.right = delete(root.right, value);
+
+            // if value is same as root's value, then this is the node
+            // to be deleted
+        else
+        {
+
+            // node with only one child or no child
+            if ((root.left == null) || (root.right == null))
+            {
+                Node temp = null;
+                if (temp == root.left)
+                    temp = root.right;
+                else
+                    temp = root.left;
+
+                // No child case
+                if (temp == null)
+                {
+                    temp = root;
+                    root = null;
+                }
+                else // One child case
+                    root = temp; // Copy the contents of
+                // the non-empty child
+            }
+            else
+            {
+
+                // node with two children: Get the inorder
+                // successor (smallest in the right subtree)
+                Node temp = minValueNode(root.right);
+
+                // Copy the inorder successor's data to this node
+                root.value = temp.value;
+
+                // Delete the inorder successor
+                root.right = delete(root.right, temp.value);
+            }
+        }
+
+        // If the tree had only one node then return
+        if (root == null)
+            return root;
+
+        // STEP 2: UPDATE HEIGHT OF THE CURRENT NODE
+        root.height = max(height(root.left), height(root.right)) + 1;
+
+        // STEP 3: GET THE BALANCE FACTOR OF THIS NODE (to check whether
+        // this node became unbalanced)
+        int balance = getBalance(root);
+
+        // If this node becomes unbalanced, then there are 4 cases
+        // Left Left Case
+        if (balance > 1 && getBalance(root.left) >= 0)
+            return rotateRight(root);
+
+        // Left Right Case
+        if (balance > 1 && getBalance(root.left) < 0)
+        {
+            root.left = rotateLeft(root.left);
+            return rotateRight(root);
+        }
+
+        // Right Right Case
+        if (balance < -1 && getBalance(root.right) <= 0)
+            return rotateLeft(root);
+
+        // Right Left Case
+        if (balance < -1 && getBalance(root.right) > 0)
+        {
+            root.right = rotateRight(root.right);
+            return rotateLeft(root);
+        }
+
+        return root;
     }
 }
 public class Main {
